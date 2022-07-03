ab7a08a25d8f157bf24a4574bc55786809cb528f
ama-acquah <100629023+ama-acquah@users.noreply.github.com>
2022-07-03 15:49:08 -0400

    Add files via upload

---
+++ Skin Care Portfolio Project.sql
@@ -0,0 +1,52 @@
+SELECT *
+ FROM SkinCare
+
+SELECT Label, Brand, Name, Price
+ FROM SkinCare
+
+SELECT Label, Brand, Name, Price
+ FROM SkinCare
+    ORDER BY Brand, Price
+
+SELECT Label, Brand, Name, Price
+ FROM SkinCare
+    WHERE Price <= 50
+    ORDER BY Brand,Price
+-- Considered More affordable Products 
+
+SELECT Label, Brand, Name, Price
+ FROM SkinCare
+    WHERE Price >=75
+    ORDER BY Brand, Price 
+-- Considered more expensive products
+
+SELECT Brand, MAX(Price) as Most_Expensive_Product_From_Brand
+ FROM SkinCare
+    GROUP BY Brand
+    ORDER BY Brand
+
+SELECT Brand,Name, Price as Most_Expensive_Item_From_Brand, Rank
+ FROM (
+   SELECT Brand, Name, Price, Rank, CASE WHEN Price = MAX(Price) OVER(PARTITION BY Brand ORDER BY (Select 1)) THEN 'Take' ELSE 'Leave' END AS Pickup 
+   FROM SkinCare
+
+ ) X WHERE Pickup = 'Take' ORDER BY Brand
+ -- Most expensive product from each brand 
+
+ SELECT Brand,Name, Price as Most_Expensive_Item_From_Brand, Rank
+ FROM 
+ (
+   SELECT Brand, Name, Price, Rank, CASE WHEN Price = MIN(Price) OVER(PARTITION BY Brand ORDER BY (Select 1)) THEN 'Take' ELSE 'Leave' END AS Pickup 
+   FROM SkinCare
+ ) 
+ X WHERE Pickup = 'Take' ORDER BY Brand
+ -- Least expensive product from each brand 
+
+SELECT TOP 100 Brand, Name, Ingredients, Combination as Combination_Skin_Type, Dry as Dry_Skin_Type, Normal as Normal_Skin_Type, Oily as Oily_Skin_Type, Sensitive as Sensitive_Skin_Care, Price, Rank
+ FROM (
+    SELECT Brand, Name, Ingredients, Combination, Dry, Normal, Oily, Sensitive, Price, Rank, CASE WHEN Price = MAX(Price) OVER(PARTITION BY Brand ORDER BY (Select Price)) THEN 'Take' ELSE 'Leave' END AS Pickup 
+    FROM SkinCare
+ ) 
+ X WHERE Pickup = 'Take' ORDER BY Price DESC 
+ -- Top 100 most expensive products and which skin type they accomitdate 
+ 
